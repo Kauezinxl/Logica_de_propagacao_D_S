6e5d2bfde1959bcf734cd94c26f038c0d477c868
kauezinxl <kaueclashr21@gmail.com>
2024-10-03 07:27:51 -0300

    Karython

---
+++ Aula10.py
@@ -0,0 +1,5 @@
+nota = float(input('Digite a primeira nota do aluno: '))
+nota1 = float(input('Digite a segunda nota do aluno: '))
+nota2 = float(input('Digite a terceira nota do aluno: '))
+
+     
